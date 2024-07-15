f544c2382f8dbd25818afc5822ffbdea7c111a98
David Alsh <alshdavid@gmail.com>
2024-07-15 11:07:01 +1000

    bench script

--- bench.bash
+++ bench.bash
@@ -10,9 +10,9 @@
 # echo "GET http://localhost:8080" | vegeta attack -rate="${RATE}" -duration=5s | vegeta report --type=text >> results/${RATE}_${NAME}.html
 # echo "</pre></code>" >> results/${RATE}_${NAME}.html
 
-REQUESTS=100000
+REQUESTS=1000000
 PORT=8080
 URL="http://localhost:${PORT}"
 oha -n 10000 $URL
 reset
-oha -n $REQUESTS -c 100 --latency-correction -disable-keepalive $URL
+oha -n $REQUESTS -c 1000 --latency-correction -disable-keepalive $URL

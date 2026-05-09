0d29f9d7f22ab0ba240297641cc5467f8dd8b450
David Alsh <alshdavid@gmail.com>
2026-05-09 12:48:40 +1000

    Readme

--- examples/basic_http2/src/main.rs
+++ examples/basic_http2/src/main.rs
@@ -10,14 +10,15 @@ use uhttp::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+  let cert_path =
+    PathBuf::from(std::env::var("SSL_CERT_PATH").context("Missing SSL_CERT_PATH env var")?);
+  let key_path =
+    PathBuf::from(std::env::var("SSL_KEY_PATH").context("Missing SSL_KEY_PATH env var")?);
+
   uhttp::http2::create_server(
     uhttp::http2::Http2ServerOptions {
-      cert_path: Some(PathBuf::from(
-        std::env::var("SSL_CERT_PATH").context("Missing SSL_CERT_PATH env var")?,
-      )),
-      key_path: Some(PathBuf::from(
-        std::env::var("SSL_KEY_PATH").context("Missing SSL_KEY_PATH env var")?,
-      )),
+      cert_path: Some(cert_path),
+      key_path: Some(key_path),
     },
     |_req, mut res| async move {
       res.header().add("Content-Type", "text/html").await?;

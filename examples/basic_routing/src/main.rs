7ae4624b95aa787b866e107053976db5ee3575be
David Alsh <alshdavid@gmail.com>
2026-05-24 14:38:21 +1000

    Small fixes

--- examples/basic_routing/src/main.rs
+++ examples/basic_routing/src/main.rs
@@ -1,5 +1,3 @@
-use std::sync::Arc;
-
 /*
   Test with:
   curl http://localhost:8080
@@ -9,63 +7,26 @@ use std::sync::Arc;
 */
 use uhttp::*;
 
-#[derive(Debug, Clone)]
-struct Context {
-  test: Option<String>,
-  service: Arc<String>,
-}
-
-async fn middleware_logger<T>(
-  req: uhttp::Request,
-  res: uhttp::Response,
-  ctx: T,
-) -> uhttp::Result<Option<(uhttp::Request, uhttp::Response, T)>> {
-  println!("[{}] {}", req.method(), req.uri().path());
-  Ok(Some((req, res, ctx)))
-}
-
-async fn middleware_value_set(
-  req: uhttp::Request,
-  res: uhttp::Response,
-  mut ctx: Context,
-) -> uhttp::Result<Option<(uhttp::Request, uhttp::Response, Context)>> {
-  ctx.test = Some("something".to_string());
-  Ok(Some((req, res, ctx)))
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-  let context = Context {
-    test: None,
-    service: Arc::new("OK".to_string()),
-  };
+  let mut app = uhttp::router::Router::new_without_context();
 
-  let mut app = uhttp::router::Router::new(context);
+  app.get("/foo", |_req, mut res, _ctx| async move {
+    res.write(b"foo\n").await?;
+    Ok(())
+  });
 
-  app.with_all(middleware_logger);
-  // app.with_all(middleware_value_set);
-
-  app
-    .with(middleware_value_set)
-    .get("/foo", |_req, mut res, ctx| async move {
-      dbg!(&ctx);
-      println!("hiii");
-      res.write(b"foo\n").await?;
-      Ok(())
-    });
-
-  app.post("/bar", |_req, mut res, ctx| async move {
+  app.post("/bar", |_req, mut res, _ctx| async move {
     res.write(b"bar\n").await?;
     Ok(())
   });
 
-  app.get("/bar", |_req, mut res, ctx| async move {
-    dbg!(&ctx);
+  app.get("/bar", |_req, mut res, _ctx| async move {
     res.write(b"bar\n").await?;
     Ok(())
   });
 
-  app.get("/fizz/:buzz", |req, mut res, ctx| async move {
+  app.get("/fizz/:buzz", |req, mut res, _ctx| async move {
     res.write(b"fizz\n").await?;
 
     let Some(buzz) = req.url_param("buzz") else {
@@ -77,7 +38,7 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
   });
 
-  app.get("/*", |_req, mut res, ctx| async move {
+  app.get("/*", |_req, mut res, _ctx| async move {
     res.write(b"Not found route").await?;
     Ok(())
   });
